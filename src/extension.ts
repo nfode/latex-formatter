5440878deb2b718666db4e2019daa78465fbb6eb
Nick Fode <nickfode@posteo.de>
2017-12-13 23:37:16 +0100

    Add various improvements
    Add:
    - better logging
    
    Remove:
    - unneccessary if-else

--- src/extension.ts
+++ src/extension.ts
@@ -1,120 +1,128 @@
-'use strict';
+"use strict"
 
-import * as vscode from 'vscode';
-import cp = require('child_process');
-import path = require('path');
-import os = require('os');
-var channel = null;
+import * as vscode from "vscode"
+import cp = require("child_process")
+import path = require("path")
+import os = require("os")
 
-const fullRange = doc => doc.validateRange(new vscode.Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE));
-const LATEX_MODE: vscode.DocumentFilter = { language: 'latex', scheme: 'file' };
-
+const LATEX_MODE: vscode.DocumentFilter = { language: "latex", scheme: "file" }
+let channel: vscode.OutputChannel = null
+const formatter: string = "latexindent"
 export class OperatingSystem {
-    public name;
-    public file_ext;
-    public checker;
+    public name
+    public file_ext
+    public checker
     constructor(name: string, file_ext: string, checker: string) {
-        this.name = name;
-        this.file_ext = file_ext;
-        this.checker = checker;
+        this.name = name
+        this.file_ext = file_ext
+        this.checker = checker
     }
 }
-const windows: OperatingSystem = new OperatingSystem('win32', '.exe', 'where');
-const linux: OperatingSystem = new OperatingSystem('linux', '.pl', 'which');
-const mac: OperatingSystem = new OperatingSystem('darwin', '.pl', 'which');
+let operatingSystems: OperatingSystem[] = []
+operatingSystems[0] = new OperatingSystem("win32", ".exe", "where")
+operatingSystems[1] = new OperatingSystem("linux", ".pl", "which")
+operatingSystems[2] = new OperatingSystem("darwin", ".pl", "which")
 
 export class LaTexFormatter {
-    private machine_os: string;
-    private current_os: OperatingSystem;
-    private formatter: string;
+    private machine_os: string
+    private current_os: OperatingSystem
+    private formatter: string
     constructor() {
-        this.machine_os = os.platform();
-        console.log(this.machine_os);
-        this.formatter = 'latexindent';
+        this.machine_os = os.platform()
+        console.log(this.machine_os)
     }
     public formatDocument(document: vscode.TextDocument): Thenable<vscode.TextEdit[]> {
         return new Promise((resolve, reject) => {
-            let formatter = 'latexindent';
-            let filename = document.fileName;
+            let filename = document.fileName
+            this.current_os = operatingSystems.filter(item => item.name == this.machine_os)[0]
+            if(this.current_os == null) return reject("Unsupported operating system.")
 
-            if (this.machine_os == windows.name) {
-                this.current_os = windows;
-            } else if (this.machine_os == linux.name) {
-                this.current_os = linux;
-            }  else if (this.machine_os == mac.name) {
-                this.current_os = mac;
-            }
-
-            this.checkPath(this.current_os.checker).then((res) => {
-                if (!res) {
-                    showErrorMessage('Can not find latexindent in PATH!');
-                    return resolve(null);
-                }
-                this.format(filename, document).then((res) => {
-                    return resolve(res);
-                });
-
-            });
-        });
-    }
-    private checkPath(checker: string): Thenable<boolean> {
-        return new Promise((resolve, reject) => {
-            cp.exec(checker + ' ' + this.formatter, (err, stdout, stderr) => {
-                if (stdout == '') {
-                    this.formatter += this.current_os.file_ext;
-                    this.checkPath(checker).then((res) => {
-                        if (res) resolve(true);
-                        else resolve(false);
-                    });
-                }
-                resolve(true);
-            });
+            this.checkPath(this.current_os.checker)
+                .then(res => {
+                    if (!res) {
+                        showErrorMessage("Can not find the latexindent executable in PATH!")
+                        reject()
+                    }
+                    return this.format(filename, document)
+                })
+                .then(
+                    res => {
+                        return resolve(res)
+                    },
+                    err => {
+                        console.log(err)
+                        return reject(err)
+                    }
+                )
         })
-
+    }
+    private checkPath(checker: string): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            cp.exec(checker + " " + formatter, (err, stdout, stderr) => {
+                if (stdout == "") {
+                    this.formatter += this.current_os.file_ext
+                    this.checkPath(checker).then(res => {
+                        if (res) resolve(true)
+                        else resolve(false)
+                    })
+                }
+                resolve(true)
+            })
+        })
     }
     private format(filename: string, document: vscode.TextDocument): Thenable<vscode.TextEdit[]> {
         return new Promise((resolve, reject) => {
-            cp.exec(this.formatter + ' "' + filename + '"', (err, stdout, stderr) => {
-                if (stdout != '') {
-                    var edit = [vscode.TextEdit.replace(fullRange(document), stdout)];
-                    return resolve(edit);
+            cp.exec(formatter + ' "' + filename + '"', (err, stdout, stderr) => {
+                if (stdout != "") {
+                    const documentStart = new vscode.Position(0, 0)
+                    const documentEnd = document.lineAt(document.lineCount - 1).range.end
+                    let edit = [
+                        new vscode.TextEdit(new vscode.Range(documentStart, documentEnd), stdout)
+                    ]
+                    return resolve(edit)
+                } else {
+                    return reject(stderr)
                 }
-                return resolve(null);
-            });
-        });
-
+            })
+        })
     }
 }
 
-function showErrorMessage(msg:string) {
-    vscode.window.showErrorMessage(msg);
+function showErrorMessage(msg: string) {
+    vscode.window.showErrorMessage(msg)
 }
 
 class LaTexDocumentRangeFormatter implements vscode.DocumentFormattingEditProvider {
-    private formatter: LaTexFormatter;
+    private formatter: LaTexFormatter
 
     constructor() {
-        this.formatter = new LaTexFormatter();
+        this.formatter = new LaTexFormatter()
     }
     public provideDocumentFormattingEdits(
         document: vscode.TextDocument,
-        options: vscode.FormattingOptions, token: vscode.CancellationToken):
-        Thenable<vscode.TextEdit[]> {
-        return document.save().then(() => {
-            return this.formatter.formatDocument(document);
-        });
-
+        options: vscode.FormattingOptions,
+        token: vscode.CancellationToken
+    ): Thenable<vscode.TextEdit[]> {
+        return this.formatter.formatDocument(document).then(null, err => {
+            showErrorMessage("latex-formatter: An error occurred please check the output.")
+            channel.append(err)
+            channel.appendLine("")
+            channel.show()
+            return []
+        })
     }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    channel = vscode.window.createOutputChannel('latex-formatter');
+    channel = vscode.window.createOutputChannel("latex-formatter")
 
     context.subscriptions.push(
         vscode.languages.registerDocumentFormattingEditProvider(
-            LATEX_MODE, new LaTexDocumentRangeFormatter()));
+            LATEX_MODE,
+            new LaTexDocumentRangeFormatter()
+        )
+    )
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
